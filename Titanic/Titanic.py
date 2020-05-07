ae0b810eb89fc91f05f76df8d77deb1d5b3ed38b
Miguel Angel Diaz de la Campa <mdiazcampa@live.com>
2020-05-07 23:43:49 +0200

    Ultima implementacion

--- Titanic/Titanic.py
+++ Titanic/Titanic.py
@@ -1,10 +1,14 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
 
 import pandas 
 import os
 import matplotlib.pyplot as mathPlot
+import numpy 
 import seaborn as sea
 import difflib 
-
+#define a class to help us with the process
 class ModelFileHelper(object):
     """Ayuda a dar una descripcion de un fichero y a su carga """
     def __init__(self, csvFile):
@@ -32,9 +36,7 @@ class ModelFileHelper(object):
     def __translateTypestoHumanReadable(self, text):
         return text.replace("int64", "Numero").replace("object", "Cadena de texto AlfaNumerica").replace("float64", "Numero (largo)")
 
-     
-
-#load the train model :
+#load the train model and store it in a dictionary :
 files = { "train" : ModelFileHelper("dataInputs/train.csv"), "test" :   ModelFileHelper("dataInputs/test.csv")} 
  
 #describe both files 
@@ -58,7 +60,70 @@ for index, (clave, valor) in enumerate (files.items()):
     print ("File: " + valor.fileName)
     valor.csvFile.info()
     print (valor.getDescription())  
-fig, axs = mathPlot.subplots(ncols=5, figsize=(30,5))
-sea.violinplot(x="Survived", y="Age", hue="Sex", data=files.get("train").csvFile, ax=axs[0])
- 
- 
+
+# %%
+#Correlations
+print ("*****************************************************************")
+print ("Limpieza del modelo: análisis de las correlaciones ")   
+print ("*****************************************************************")
+files.get("train").csvFile.corr()
+
+
+
+# %%
+#Clean up Cabin Column
+print ("*****************************************************************")
+print ("Limpieza del modelo: Eliminando campos Irrelevantes ")   
+print ("*****************************************************************")
+for index, (clave, valor) in enumerate (files.items()):
+    print ("...............................................................")
+    print ("File: " + valor.fileName)
+    print ("Removing Column Cabin: ")
+    valor.csvFile.drop('Cabin', axis=1, inplace=True)
+    print ("Removing Column Embarked: ")
+    valor.csvFile.drop('Embarked', axis=1, inplace=True)
+    print (valor.getModelTypeDetail())  
+
+
+# %%
+#Infer missing age Data
+print ("*****************************************************************")
+print ("Limpieza del modelo: Inferir campos de Edad vacios:")   
+print ("*****************************************************************") 
+files.get("train").csvFile.query('Age >=1').groupby(['Survived','Pclass', 'Sex']).agg({'Age': ['mean', 'min', 'max']})
+
+
+# %%
+#Comparative of age groups between train model and test model
+print ("*****************************************************************")
+print ("Limpieza del modelo: comparacion de grupos de edad :")   
+print ("*****************************************************************") 
+print ("modelo Train:")
+files.get("train").csvFile.query('Age >=1').groupby(['Pclass', 'Sex']).agg({'Age': ['mean', 'min', 'max']})
+
+
+# %%
+#imp
+print ("modelo test:")
+files.get("test").csvFile.query('Age >=1').groupby(['Pclass', 'Sex']).agg({'Age': ['mean', 'min', 'max']})
+
+
+# %%
+fig, axs = mathPlot.subplots(ncols=2, figsize=(30,5))
+sea.pointplot(x="Pclass", y="Age", hue="Sex", data=  files.get("train").csvFile.query('Age >= 1'), ax=axs[0])
+sea.pointplot(x="Pclass", y="Age", hue="Sex", data=  files.get("test").csvFile.query('Age >= 1'), ax=axs[1])
+
+
+# %%
+print ("*****************************************************************")
+print ("Limpieza del modelo: corrección de de la edad :")   
+print ("*****************************************************************") 
+print ("modelo Train:")
+#estadisticasTrain= files.get("train").csvFile.query('Age >=1').groupby(['Survived','Pclass', 'Sex']).agg({'Age': ['mean', 'min', 'max']})
+nan= numpy.nan
+print (files.get("train").csvFile.query('(Age.isnull()) | (Age < 0)', engine='python'))
+
+
+# %%
+
+
